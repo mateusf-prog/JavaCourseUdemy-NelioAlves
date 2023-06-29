d53c4b1e5a4aca5c734bc43ff14a52c44ec12d19
mateusf-prog <mateus102006@hotmail.com>
2023-06-29 11:55:43 -0300

    consumer #1

---
+++ section_20_functional-programming/Consumer/src/application/Main.java
@@ -0,0 +1,23 @@
+package application;
+
+import entities.Product;
+import util.PriceUpdate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public  class Main {
+    public static void main(String[] args) {
+
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("tv", 900.0));
+        list.add(new Product("notebook", 1550.0));
+        list.add(new Product("tablet", 700.0));
+        list.add(new Product("HD case", 80.0));
+
+        list.forEach(new PriceUpdate());
+
+        list.forEach(System.out::println);
+    }
+}
