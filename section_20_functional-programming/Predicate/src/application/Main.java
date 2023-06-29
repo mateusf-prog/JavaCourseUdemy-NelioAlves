301969396f67a64fa7d7101eb1d83ffe7ca7196a
mateusf-prog <mateus102006@hotmail.com>
2023-06-29 11:18:01 -0300

    predicate #1

---
+++ section_20_functional-programming/Predicate/src/application/Main.java
@@ -0,0 +1,25 @@
+package application;
+
+import entities.Product;
+import util.ProductPredicate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("tv", 900.0));
+        list.add(new Product("tablet", 950.0));
+        list.add(new Product("Mouse", 50.0));
+        list.add(new Product("HD case", 80.0));
+
+        list.removeIf(new ProductPredicate());
+
+        for (Product p : list) {
+            System.out.println(p);
+        }
+    }
+}
