a20946844c38128c614223f7c92fa70e0779cb56
mateusf-prog <mateus102006@hotmail.com>
2023-06-30 09:18:12 -0300

    Function #1

---
+++ section_20_functional-programming/Function/src/application/Main.java
@@ -0,0 +1,29 @@
+package application;
+
+import entities.Product;
+import util.UpperCaseName;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("Tv", 1000.0));
+        list.add(new Product("Tablet", 750.0));
+        list.add(new Product("Notebook", 1500.0));
+        list.add(new Product("HD case", 85.0));
+
+        // apply on each element of the list, an instance of UpperCaseName and convert the stream to list
+        List<String> names = list.stream().map(new UpperCaseName()).collect(Collectors.toList());
+
+        // list.stream = convert the list to stream
+        // .map(function) = apply on each element of the list, an instance of UpperCaseName
+        // .collect(Collector.toList()) = return stream to list
+
+        names.forEach(System.out::println);
+    }
+}
