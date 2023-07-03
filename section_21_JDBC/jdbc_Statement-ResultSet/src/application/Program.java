9c309a855bc1bedf26cd9f784c7f8c023a10b38f
mateusf-prog <mateus102006@hotmail.com>
2023-07-03 10:47:52 -0300

    Statement and ResultSet

--- section_21_JDBC/jdbc_Statement-ResultSet/src/application/Program.java
+++ section_21_JDBC/jdbc_Statement-ResultSet/src/application/Program.java
@@ -3,12 +3,32 @@ package application;
 import db.DB;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Program {
     public static void main(String[] args) {
 
-        // teste connection with database
-        Connection conn = DB.getConnection();
-        DB.closeConnection();
+        Connection conn = null;
+        Statement st = null;
+        ResultSet rs = null;
+
+        try {
+            conn = DB.getConnection();  // starting connection with database
+            st = conn.createStatement(); // instantiating a object Statement with my connection
+            rs = st.executeQuery("select * from department"); // table object
+
+            while (rs.next()) {
+                System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        finally {
+            DB.closeResultSet(rs);
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
     }
 }
