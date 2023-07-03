c1d18ef3f13114925964ae65e76e2240ae48fabd
mateusf-prog <mateus102006@hotmail.com>
2023-07-03 13:49:16 -0300

    update data jdbc

---
+++ section_21_JDBC/jdbc_Update-data/src/application/Program.java
@@ -0,0 +1,40 @@
+package application;
+
+import db.DB;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Connection conn = null;
+        PreparedStatement st = null;
+
+        /* Open connection
+        *  defines the changes(update)
+        *  save the rows affected in a variable and after printing
+        *  treats the possible exceptions
+        *  close the connections */
+        try {
+            conn = DB.getConnection();
+            st = conn.prepareStatement(
+                    "UPDATE seller "
+                    + "SET BaseSalary = BaseSalary + ? "
+                    + "WHERE "
+                    + "(DepartmentId = ?)");
+            st.setDouble(1, 200.0);
+            st.setInt(2, 2);
+
+            int rowsAffected = st.executeUpdate();
+
+            System.out.println("Done! Rows affected: " + rowsAffected);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
+    }
+}
